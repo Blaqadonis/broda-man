44ce3d8f18462f80b85dc269e0e7565ce974cdb0
Blaq <chinonsoodiaka@gmail.com>
2023-11-14 00:28:09 +0100

    CICD Integration another one

--- src/eval.py
+++ src/eval.py
@@ -8,7 +8,7 @@ from nltk.translate.bleu_score import sentence_bleu
 # OpenAI API key for generating the evaluation dataset
 if os.getenv("OPENAI_API_KEY") is None:
     if any(['VSCODE' in x for x in os.environ.keys()]):
-        print('Please enter password in the VS Code prompt at the top of your VS Code window!')
+        print('Please enter the password in the VS Code prompt at the top of your VS Code window!')
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Paste your OpenAI Key from: https://platform.openai.com/account/api-keys\n")
     openai.api_key = os.getenv("OPENAI_API_KEY", "")
 
@@ -21,23 +21,7 @@ wandb.init(project="evaluation-dataset")
 # Locations and destinations for evaluation
 evaluation_data = [
     {"location": "Ikeja", "destination": "National Stadium"},
-    {"location": "Lekki", "destination": "Ojo"},
-    {"location": "Masha", "destination": "Doyin"},
-    {"location": "Surulere", "destination": "Badagry"},
-    {"location": "Ikeja", "destination": "National Stadium"},
-    {"location": "Lekki", "destination": "Ojo"},
-    {"location": "Masha", "destination": "Doyin"},
-    {"location": "Berger", "destination": "Festac"},
-    {"location": "Computer Village", "destination": "Maryland"},
-    {"location": "Iponri", "destination": "Aguda"},
-    {"location": "Mile-2", "destination": "Iyana-Ipaja"},
-    {"location": "Iyana Ipaja", "destination": "Costain"},
-    {"location": "Epe", "destination": "Ebute-Meta"},
-    {"location": "Costain", "destination": "LUTH"},
-    {"location": "Idi-Araba", "destination": "Doyin"},
-    {"location": "Admiralty Road, Lekki Phase 1", "destination": "Lekki, Phase 2"},
-    {"location": "Ikeja", "destination": "Yaba"},
-    {"location": "Ojo Barracks", "destination": "Trade-Fair"}
+    # Add more locations and destinations as needed
 ]
 
 # An empty list to store evaluation examples
@@ -54,26 +38,7 @@ for data in evaluation_data:
     ]
 
     # Modify the expected assistant reply for each location-destination pair
-    expected_reply = ""
-
-    if location == "Ikeja" and destination == "National Stadium":
-        expected_reply = "Na straight na. Take Mobolaji Bank Anthony Way, then turn right onto Ikorodu Road. Follow Ikorodu Road all the way to the stadium."
-    elif location == "Lekki" and destination == "Ojo":
-        expected_reply = "Take Lekki-Epe Expressway to Ozumba Mbadiwe Avenue. Then turn left onto the Third Mainland Bridge. Follow the Third Mainland Bridge all the way to Ojo."
-    elif location == "Masha" and destination == "Doyin":
-        expected_reply = "Take Apapa-Oshodi Expressway to Costain. Then turn left onto Ijora Causeway. Follow Ijora Causeway to Iganmu. Then turn right onto Doyin Street."
-    elif location == "Surulere" and destination == "Badagry":
-        expected_reply = "Take Oshodi-Apapa Expressway to Badagry Expressway. Follow Badagry Expressway all the way to Badagry."
-    elif location == "Berger" and destination == "Festac":
-        expected_reply = "Take Lagos-Abeokuta Expressway to Festac Town Exit. Then turn left onto Festac Link Road. Follow Festac Link Road to Festac Town."
-    elif location == "Computer Village" and destination == "Maryland":
-        expected_reply = "Take Ikorodu Road to Ikeja Under Bridge. Then turn left onto Mobolaji Bank Anthony Way. Follow Mobolaji Bank Anthony Way to Maryland."
-    elif location == "Iponri" and destination == "Aguda":
-        expected_reply = "Take Lagos Island Ring Road to Fatai Atere Way. Then turn right onto Igbosere Road. Follow Igbosere Road to Aguda."
-    elif location == "Mile-2" and destination == "Iyana-Ipaja":
-        expected_reply = "Take Lagos-Badagry Expressway to Iyana-Ipaja Exit. Then turn right onto Iyana-Ipaja Road. Follow Iyana-Ipaja Road to Iyana-Ipaja."
-    elif location == "Iyana Ipaja" and destination == "Costain":
-        expected_reply = "Take Iyana-Ipaja Road to Lagos-Abeokuta Expressway. Then turn left onto Oshodi-Apapa Expressway. Follow Oshodi-Apapa Expressway all the way to Costain."
+    expected_reply = f"Modify this with the expected reply for {location} to {destination}"
 
     evaluation_example = {
         "messages": user_messages,
@@ -98,7 +63,6 @@ wandb.run.log_artifact(artifact)
 
 print(f"Evaluation dataset saved to 'evaluation_dataset.jsonl' with {len(evaluation_examples)} examples.")
 
-
 # Hardcoded initial part of the model ID
 base_model_id = "ft:gpt-3.5-turbo-0613:personal:broda-man:"
 
@@ -110,11 +74,6 @@ model_id = base_model_id + model_id_suffix
 
 # Initialize Weights & Biases for model evaluation
 wandb.init(project="evaluating-new-model", entity="blaq")
-
-# Export WandB entity, project, and API key to the environment
-os.environ["WANDB_ENTITY"] = wandb.run.entity
-os.environ["WANDB_PROJECT"] = wandb.run.project
-os.environ["WANDB_API_KEY"] = wandb.api.api_key
 
 # Retrieve the evaluation dataset from WandB
 artifact = wandb.run.use_artifact("evaluation_dataset:latest")
@@ -176,14 +135,10 @@ wandb.log({"evaluation_results": eval_results})
 # Finish Weights & Biases run
 wandb.finish()
 
-
 # Get the Weights & Biases link
 wandb_link = wandb.run.get_url()
 
 # Save the Weights & Biases link to an environment variable
-#with open(os.environ['WANDB_LINK'], 'a') as f:
- #           print(f'REPORT_URL={wandb_link}', file=f)
-
 os.environ["WANDB_LINK"] = wandb_link
 
 print("Evaluation completed. Results logged to Weights & Biases.")
