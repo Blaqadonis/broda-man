93142f43dfe04b5b090fd87bda30dcf2f5a9396c
Blaq <chinonsoodiaka@gmail.com>
2023-11-26 09:06:48 +0100

    CICD Integration

--- src/eval.py
+++ src/eval.py
@@ -2,25 +2,52 @@ import json
 import os
 import openai
 import getpass
-import wandb
 from nltk.translate.bleu_score import sentence_bleu
 
 # OpenAI API key for generating the evaluation dataset
-if os.getenv("OPENAI_API_KEY") is None:
+openai.api_key = os.getenv("OPENAI_API_KEY", "")
+
+# Ensure that the OpenAI API key is set
+if not openai.api_key:
     if any(['VSCODE' in x for x in os.environ.keys()]):
         print('Please enter the password in the VS Code prompt at the top of your VS Code window!')
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Paste your OpenAI Key from: https://platform.openai.com/account/api-keys\n")
     openai.api_key = os.getenv("OPENAI_API_KEY", "")
 
-assert os.getenv("OPENAI_API_KEY", "").startswith("sk-"), "This doesn't look like a valid OpenAI API key"
+assert openai.api_key.startswith("sk-"), "This doesn't look like a valid OpenAI API key"
 print("OpenAI API key configured")
-# Set up WandB project and run for generating the evaluation dataset
-run = wandb.init(project="evaluation")
 
 # Locations and destinations for evaluation
 evaluation_data = [
     {"location": "Ikeja", "destination": "National Stadium"},
-    # Add more locations and destinations as needed
+    {"location": "Lekki", "destination": "Eko Hotel"},
+    {"location": "Surulere", "destination": "University of Lagos"},
+    {"location": "Victoria Island", "destination": "Lekki Conservation Centre"},
+    {"location": "Yaba", "destination": "Murtala Muhammed Airport"},
+    {"location": "Ikorodu", "destination": "The Palms Shopping Mall"},
+    {"location": "Ajao Estate", "destination": "Ikeja City Mall"},
+    {"location": "Ojota", "destination": "Computer Village"},
+    {"location": "Apapa", "destination": "Tin Can Island Port"},
+    {"location": "Oshodi", "destination": "Mushin Market"},
+    {"location": "Agege", "destination": "New Garage"},
+    {"location": "Badagry", "destination": "Badagry Heritage Museum"},
+    {"location": "Epe", "destination": "Epe Fish Market"},
+    {"location": "Ikoyi", "destination": "Ikoyi Club 1938"},
+    {"location": "Makoko", "destination": "Makoko Floating School"},
+    {"location": "Obalende", "destination": "Tafawa Balewa Square"},
+    {"location": "Ijora", "destination": "Iganmu Industrial Estate"},
+    {"location": "Alimosho", "destination": "Egbeda Akowonjo Roundabout"},
+    {"location": "Gbagada", "destination": "Anthony Village"},
+    {"location": "Festac", "destination": "Festac Town Link Bridge"},
+    {"location": "Isolo", "destination": "Aswani Market"},
+    {"location": "Igando", "destination": "Igando General Hospital"},
+    {"location": "Oshodi-Isolo", "destination": "Cele Expressway"},
+    {"location": "Amuwo-Odofin", "destination": "Apple Junction"},
+    {"location": "Agbado", "destination": "Agbado Crossing"},
+    {"location": "Alakuko", "destination": "Ijaiye Market"},
+    {"location": "Egbeda", "destination": "MicCom Golf Course"},
+    {"location": "Ilupeju", "destination": "Coker Market"},
+    {"location": "Magodo", "destination": "Magodo Phase II"},
 ]
 
 # An empty list to store evaluation examples
@@ -46,62 +73,41 @@ for data in evaluation_data:
 
     evaluation_examples.append(evaluation_example)
 
-# Log evaluation examples to WandB
-wandb.log({"evaluation_examples": evaluation_examples})
-
 # Save the evaluation dataset to a JSON file
 with open("evaluation_dataset.jsonl", "w") as jsonl_file:
     for example in evaluation_examples:
         json.dump(example, jsonl_file)
         jsonl_file.write("\n")
 
-# Log the evaluation dataset to WandB
-artifact = wandb.Artifact(name="evaluation_dataset", type="dataset")
-artifact.add_file("evaluation_dataset.jsonl")
-wandb.run.log_artifact(artifact)
-
 print(f"Evaluation dataset saved to 'evaluation_dataset.jsonl' with {len(evaluation_examples)} examples.")
-
-# Hardcoded initial part of the model ID
-base_model_id = "ft:gpt-3.5-turbo-0613:personal:broda-man:"
 
 
 # Import the fine-tuning model ID suffix from the environment
-model_id_suffix = os.getenv('BRODAMAN_FINETUNE_MODEL_SUFFIX', '')
-#model_id_suffix = "8MMdTENP"
-# Concatenate the initial part and the extracted model ID suffix
-model_id = base_model_id + str(model_id_suffix)
+model_id = os.getenv('BRODAMAN_FINETUNE_MODEL_ID', '')
+# model_id_suffix = "8MMdTENP"
 
-# Initialize Weights & Biases for model evaluation
-#run = wandb.init(project="evaluating-new-model", entity="blaq")
-
-
-# Retrieve the evaluation dataset from WandB
-artifact = wandb.run.use_artifact(artifact)
-artifact_dir = artifact.download()
-
-# Load the evaluation dataset
-with open(os.path.join(artifact_dir, "evaluation_dataset.jsonl"), "r") as jsonl_file:
-    evaluation_data = [json.loads(line) for line in jsonl_file]
+# Initialize OpenAI for model evaluation
+openai.api_key = openai.api_key  # Use the updated API key
+openai.organization = None  # Remove the organization parameter if not using an organization
 
 # Evaluate the model
 eval_results = []
 
-for example in evaluation_data:
+for example in evaluation_examples:
     location = example["messages"][0]["content"].split(":")[1].strip()
     destination = example["messages"][1]["content"].split(":")[1].strip()
     expected_reply = example["expected_reply"]
 
     # Generate a response from the model using ChatCompletion
     user_messages = [
-        {"role": "system", "content": "You are Broda-man, the Lagos state traffic bot. You assist users who want to beat traffic Lagos at all costs, by providing them with routes with less traffic when they provide you with their location and destination details. You respond strictly and only in Nigerian pidgin language. You are often cheerful too."},
+        {"role": "system",
+         "content": "You are Broda-man, the Lagos state traffic bot. You assist users who want to beat traffic Lagos at all costs, by providing them with routes with less traffic when they provide you with their location and destination details. You respond strictly and only in Nigerian pidgin language. You are often cheerful too."},
         {"role": "user", "content": f"Location: {location}, Destination: {destination}"}
     ]
     response = openai.ChatCompletion.create(
         model=model_id,
         messages=user_messages,
-        max_tokens=50  # Adjust max tokens as needed
-    )
+        max_tokens=60)
 
     model_output = response['choices'][0]['message']['content'].strip()
 
@@ -130,20 +136,4 @@ for example in evaluation_data:
 with open("evaluation_results.json", "w") as f:
     json.dump(eval_results, f, indent=4)
 
-# Log results to Weights & Biases
-wandb.log({"evaluation_results": eval_results})
-
-# Finish Weights & Biases run
-wandb.finish()
-
-# Get the Weights & Biases link
-WANDB_LINK = run.get_url()
-
-# Save the Weights & Biases link to an environment variable
-os.environ["WANDB_LINK"] = WANDB_LINK
-print("Link: ", WANDB_LINK)
-print("Evaluation completed. Results logged to Weights & Biases.")
-
-# Save the Weights & Biases link to the GITHUB_OUTPUT file
-with open(os.environ['GITHUB_OUTPUT'], 'a') as f:
-    print(f'WANDB_LINK={WANDB_LINK}', file=f)
+print("Evaluation completed. Results logged to 'evaluation_results.json'.")
